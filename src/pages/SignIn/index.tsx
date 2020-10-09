fee79edc740b8e979612506281afacf9d04a4c42
Henrique Lopes <henrique.henriquelopess@gmail.com>
2020-10-08 22:05:04 -0300

    Finalizando integração com unform

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useCallback, useRef} from 'react';
 import {View, Image, ScrollView, KeyboardAvoidingView, Platform} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import {useNavigation} from '@react-navigation/native';
+import {Form} from '@unform/mobile';
+import {FormHandles} from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -13,20 +15,28 @@ import {Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButto
 
 const SignIn: React.FC = ()=>{
     const navigation = useNavigation();
+    const formRef = useRef<FormHandles>(null);
+
+    const handleSignIn = useCallback((data:object)=>{
+        console.log(data);
+    }, []);
+
     return(
         <>
             <KeyboardAvoidingView style={{flex: 1}} behavior={Platform.OS === "ios" ? "padding" : undefined} enabled>
                 <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{flex:1}}>
                     <Container>
                         <Image source={logoImg} />
+
                         <View>
                             <Title>Faça seu logon</Title>
                         </View>
 
-                        <Input name="email" icon="mail" placeholder="Email"/>
-                        <Input name="password" icon="lock" placeholder="Senha"/>
-
-                        <Button onPress={()=>{}}>Entrar</Button>
+                        <Form ref={formRef} onSubmit={handleSignIn}>
+                            <Input name="email" icon="mail" placeholder="Email" autoCorrect={false} autoCapitalize="none" keyboardType="url" />
+                            <Input name="password" icon="lock" placeholder="Senha" secureTextEntry/>
+                        </Form>
+                        <Button onPress={()=>{formRef.current?.submitForm()}}>Entrar</Button>
 
                         <ForgotPassword onPress={()=>{}}>
                             <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
